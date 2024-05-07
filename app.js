fa6f7a62df1ac13fa9b1323203f8861682e5497d
Ashless Scythe <drakesav@gmail.com>
2024-05-07 20:13:01 +0000

    initial setup

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const path = require("path");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello, world!',
-  })
-})
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "public"));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { url: process.env.REDIR_URL });
+});
 
 app.listen(port, () => {
-  console.log(`App is listening on port ${port}`)
-})
+  console.log(`App is listening on port ${port}`);
+});
